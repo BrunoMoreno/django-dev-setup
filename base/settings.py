d416e2b1b5f9b25026d850fed2587e1d5e789543
Bruno Queiroz <brunowlinux@gmail.com>
2019-06-25 10:36:18 -0300

    Core app added

--- base/settings.py
+++ base/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/'
+
+DATE_FORMAT = "d-m-Y"
